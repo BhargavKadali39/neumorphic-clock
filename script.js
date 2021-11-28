bf65913839aca04421d9b1ff9552b2e22886e8ab
Bhargav <bhargavkadali39@gmail.com>
2021-11-28 18:10:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 
+setInterval(myFunction, 1000);
+
+function myFunction() {
+    var today = new Date();
+    var hr = today.getHours()
+    var min = today.getMinutes()
+    var sec = today.getSeconds()
+    var am_pm = "AM"
+    console.log(time)
+    if (hr > 12) {
+        hr -= 12;
+        am_pm = "PM";
+    }
+    if (hr == 0) {
+        hr = 12;
+        am_pm = "AM";
+    }
+    let length = hr.length;
+    if (length == 1) {
+        hr = "0" + hr
+    }
+
+
+    var time = hr + " : " + min + " : " + sec + " " + am_pm;
+    var number = ""+time;
+    document.getElementById("timee").innerHTML = number;
+}
+
